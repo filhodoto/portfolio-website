ac8b14deeb17408f959201c2b79cca160be323ef
filhodoto <goncalo_ramalho@hotmail.com>
2016-01-21 22:49:02 +0000

    add github and updated home font size

--- app/index.php
+++ app/index.php
@@ -27,12 +27,8 @@
 							<li><a class="btn mail" href="mailto:meet@goncaloramalho.com">Get in touch</a></li>
 							<li><a class="btn tools" href="#">See my work</a></li>
 						</ul>
-		
-						<ol>
-							<li><a class="linkedin" href="https://www.linkedin.com/in/gtramalho" target="_blank"><span>linkedin</span></a></li>
-							<li><a class="twitter" href="https://twitter.com/_goncalo" target="_blank"><span>twitter</span></a></li>
-							<li><a class="instagram" href="https://instagram.com/goncalo_ramalho/" target="_blank"><span>instagram</span></a></li>
-						</ol>
+						<!-- call social links layout -->
+						<?php include( $root .'/layouts/social.links.php'); ?>						
 					</div>
 				</section>
 			</article>
---
+++ app/layouts/social.links.php
@@ -0,0 +1,6 @@
+<ol>
+	<li><a class="linkedin" href="https://www.linkedin.com/in/gtramalho" target="_blank"><span>linkedin</span></a></li>
+	<li><a class="twitter" href="https://twitter.com/_goncalo" target="_blank"><span>twitter</span></a></li>
+	<li><a class="instagram" href="https://instagram.com/goncalo_ramalho/" target="_blank"><span>instagram</span></a></li>
+	<li><a class="github" href="https://github.com/filhodoto" target="_blank"><span>github</span></a></li>
+</ol>
